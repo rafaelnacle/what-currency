0f07c29c81a39a8c9ccb09cb963b6c27333f9f79
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2022-05-23 17:14:46 +0000

    Get the url and return it as html

--- main.py
+++ main.py
@@ -3,14 +3,9 @@ from bs4 import BeautifulSoup
 
 
 url = "https://www.iban.com/currency-codes"
+r_iban = requests.get(url)
 
+html_iban = r_iban.text
 
-# já deixei instalado e importado o requests
-# e o BeautifulSoup.
-# a variavel com a url do iban tbm ;) 
-
-## LEIA AS DICAS DE OURO 
-
-# pode apagar esses comentários após ler
-
-# BOM DESAFIO!
+soup = BeautifulSoup(html_iban, 'html.parser')
+print(soup)
