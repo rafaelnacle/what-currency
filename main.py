c912723441da48fbba275d3e308d2c06d8919075
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2022-05-23 17:07:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://www.iban.com/currency-codes"
+
+
+# já deixei instalado e importado o requests
+# e o BeautifulSoup.
+# a variavel com a url do iban tbm ;) 
+
+## LEIA AS DICAS DE OURO 
+
+# pode apagar esses comentários após ler
+
+# BOM DESAFIO!
