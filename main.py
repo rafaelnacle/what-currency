c8a0ec58b96c441ad462c17a76a6241622e03c89
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2022-05-23 22:13:27 +0000

    User can choose a country from a list to show the type of coin they use there.

--- main.py
+++ main.py
@@ -8,4 +8,31 @@ r_iban = requests.get(url)
 html_iban = r_iban.text
 
 soup = BeautifulSoup(html_iban, 'html.parser')
-print(soup)
+rows = soup.find_all('tr')
+
+currencies = []
+id = 0
+
+for row in rows[1:]:
+    id += 1
+    contents = row.contents
+    
+    currency = {
+        "id": id,
+        "country": contents[1].string,
+        "coin": contents[3].string
+    }
+
+    currencies.append(currency)
+    print(f"#{currency['id']} {currency['country']}")
+
+try:
+    code = int(input("Choose a country code to check its coin #: "))
+
+    for currency in currencies:
+        if code == currency['id']:
+            print(f"You chose {currency['country']}")
+            print(f"The coin code is: {currency['coin']}")
+
+except ValueError as err:
+    print("Please select a number.", err)
