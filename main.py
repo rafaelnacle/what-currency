e5a8f87fdc074bf65a7d3bedf3db854a1cbf5f4d
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2022-05-24 19:18:12 +0000

    Simplified input menu

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = "https://www.iban.com/currency-codes"
-r_iban = requests.get(url)
+r_iban = requests.get("https://www.iban.com/currency-codes")
 
 html_iban = r_iban.text
 
@@ -13,26 +12,33 @@ rows = soup.find_all('tr')
 currencies = []
 id = 0
 
-for row in rows[1:]:
+for row in rows:
     id += 1
     contents = row.contents
     
     currency = {
         "id": id,
-        "country": contents[1].string,
+        "name": contents[1].string,
         "coin": contents[3].string
     }
 
     currencies.append(currency)
-    print(f"#{currency['id']} {currency['country']}")
+    print(f"#{currency['id']} {currency['name']}")
 
-try:
-    code = int(input("Choose a country code to check its coin #: "))
 
-    for currency in currencies:
-        if code == currency['id']:
-            print(f"You chose {currency['country']}")
-            print(f"The coin code is: {currency['coin']}")
+def menu():
+    try:
+        country_code = int(input("Choose a country code #: "))
 
-except ValueError as err:
-    print("Please select a number.", err)
+        if country_code > len(currencies):
+            print("This number is bigger than the list")
+            menu()
+        else:
+            country = currencies[country_code]
+            print(f"(x) {country['name']}")
+            
+    
+    except ValueError as err:
+        print("Please select a number.", err)
+
+menu()
